c39b2b6bcf4ff03b10208984a98a6b2974f76741
OlegDyachuk <34671831+OlegDyachuk@users.noreply.github.com>
2022-04-26 14:51:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+// const movies=[
+    // {
+        // title: 'Amadeus',
+        // score: 99,
+        // year: 1984
+    // },
+    // {
+        // title: 'Sharknado',
+        // score: 35,
+        // year: 2013
+    // },
+    // {
+        // title: '13 going on 30',
+        // score: 70,
+        // year: 2004
+    // }
+// ];
+// 
+// const bestMovies = movies.filter(el => {
+    // return el.year > 2000;   
+// });
+// function bestMovies(el){
+    // bestMovies.forEach(element => {
+        // console.log(element);
+    // });
+// }
+// const usernames = ['mark', 'donni', 'oleh', 'alice', 'daria', 'ghfdgdfhgftuightfhjuio', 'sfjkhtgsdjgiyvgtodg'];
+// 
+// 
+// const validUserNames = usernames.filter(el => {
+    // return el.length < 10; 
+//  });
+// function validUserNames(usernames){
+    // return usernames.filter(function(el){
+        // return el.length < 10
+    // })
+// }
+// const num = [2,1,5,9,7,8];
+// const num2 = [2,4,6,8,8,2,4,8];
+// function allEvens(arr){
+    // return arr.every(num => {
+        // return num %2 === 0;
+    // })
+// }
+// function rollDie(numSides = 10){
+    // return Math.floor(Math.random() * numSides) + 1;
+// }
+// function greet(person='Oleh', msg='Hey there', punc='!'){
+    // console.log(`${msg}, ${person}${punc}`);
+// }
+
+// const dataFromForm = {
+    // email: 'blueman@gmail.com',
+    // password: 'tobias123!',
+    // username: 'tfunke'
+// }
+// const newUser = { ...dataFromForm, id:2345, isAdmin:false}
